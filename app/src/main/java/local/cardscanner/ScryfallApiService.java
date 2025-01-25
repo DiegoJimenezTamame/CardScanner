11d36be7ed03013102183d6ace5812e36a8bc033
diegojimeneztamame <djimeneztamame@gmail.com>
2025-01-25 21:00:19 +0100

    TODO: Fix compatibility issues with the .xml resource files that generate the :app:mergeDebugResources error while compiling

--- app/src/main/java/local/cardscanner/ScryfallApiService.java
+++ app/src/main/java/local/cardscanner/ScryfallApiService.java
@@ -1,11 +1,6 @@
 package local.cardscanner;
-import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import retrofit2.http.Path;
